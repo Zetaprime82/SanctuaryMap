9fad8766d84a7b3229db0153c0249345d0c76e5f
Zetaprime82 <darklord_dm_@hotmail.com>
2023-07-04 08:16:18 +0200

    Add files via upload

---
+++ data/legionevents.js
@@ -0,0 +1,50 @@
+var legionevents = [
+  {
+    "id": 1361103,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -628.53,
+    "y": -1159.01
+  },
+  {
+    "id": 1363002,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -395.674,
+    "y": -585.139
+  },
+    {
+    "id": 1302586,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -1595.48,
+    "y": -128.734
+  },
+    {
+    "id": 1363047,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -498.19,
+    "y": 212.133
+  },
+  {
+    "id": 1363037,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -575.034,
+    "y": 1300.77
+  },
+    {
+    "id": 1361067,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -167.478,
+    "y": -262.1
+  },
+  {
+    "id": 1361090,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": 323.12,
+    "y": -423.723
+  },
+    {
+    "id": 1361004,
+    "name": "Legion Event</br>The forces of evil are amassing at this location. Decimate their ranks before they can fully organize.</br>Complete the Legion Event",
+    "x": -1757.78,
+    "y": -887.58
+  },
+]
