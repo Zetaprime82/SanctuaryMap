ff8bda00f03834dac78402f857fa3b5c437b4c2e
Zetaprime82 <darklord_dm_@hotmail.com>
2023-07-04 08:51:54 +0200

    Update worldboss.js

--- data/worldboss.js
+++ data/worldboss.js
@@ -1,31 +1,31 @@
 var worldboss = [
   {
     "id": 596773,
-    "name": "<center>World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss</center>",
+    "name": "World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss",
     "x": -907.619,
     "y": -140.395
   },
   {
     "id": 608339,
-    "name": "<center>World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss</center>",
+    "name": "World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss",
     "x": -1778.52,
     "y": 702.676
   },
   {
     "id": 798443,
-    "name": "<center>World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss</center>",
+    "name": "World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss",
     "x": -219.959,
     "y": 616.626
   },
   {
     "id": 638978,
-    "name": "<center>World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss</center>",
+    "name": "World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss",
     "x": -406.737,
     "y": -232.911
   },
   {
     "id": 608082,
-    "name": "<center>World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss</center>",
+    "name": "World Boss</br>Ashava the Pestilent, Avarice the Gold-cursed, </br>and the Wandering Death have occasionally been seen at this location.</br>Slay the World Boss",
     "x": -2071.69,
     "y": -600.054
   },
