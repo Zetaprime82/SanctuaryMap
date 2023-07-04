3a4d6b708a44680387fa50fdb7f4a61be109270e
Zetaprime82 <darklord_dm_@hotmail.com>
2023-07-04 13:12:56 +0200

    Add files via upload

---
+++ data/zones.js
@@ -0,0 +1,27 @@
+var zones = [
+  {
+    "name": "Scosglen",
+    "x": -6213,
+    "y": 117
+  },
+    {
+    "name": "Fractured Peaks",
+    "x": -4068,
+    "y": 151
+  },
+    {
+    "name": "Hawezar",
+    "x": 28,
+    "y": 215
+  },
+    {
+    "name": "Kehjistan",
+    "x": -1632,
+    "y": 189
+  },
+    {
+    "name": "Dry Steppes",
+    "x": -4233,
+    "y": 148
+  }
+]
